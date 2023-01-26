e2f27c75927abe3ebf305dea9f60eb7f6317938a
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2023-01-26 17:19:40 +0300

    Added simple code with RAII idiom.

---
+++ DEV-C220/36_leveraging_RAII_idiom.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <ostream>
+#include <utility>  // std::move
+
+#include "my_print.h"
+
+using namespace std::literals;
+
+template <typename Type>
+class ResourceManager {
+   private:
+    Type* _resourcePtr;
+
+   public:
+    ResourceManager(Type* resourcePtr) : _resourcePtr(resourcePtr){};
+
+    // запрещаем конструктор копирования
+    ResourceManager(const ResourceManager& other) = delete;
+
+    // запрещаем оператор присваивания
+    ResourceManager& operator=(const ResourceManager& other) = delete;
+
+    // разрешаем перемещающий конструктор копирования
+    // MOVE constructor
+    ResourceManager(ResourceManager&& other) {
+        // сначала забираем владение ресурсами у "other"
+        this->_resourcePtr = other._resourcePtr;
+
+        // затем обнуляем ресурсы в источнике
+        other._resourcePtr = nullptr;
+    }
+
+    // разрешаем перемещающий оператор присваивания
+    ResourceManager& operator=(ResourceManager&& other) {
+        if (this != &other) {
+            std::swap(this->_resourcePtr, other._resourcePtr);
+            delete other._resourcePtr;     // освобождаем память, на которую указывал "lhs"-указатель
+            other._resourcePtr = nullptr;  // завершаем отъем ресурса у other
+        }
+    }
+
+    ~ResourceManager() { delete _resourcePtr; }
+};
+
+class LifetimeLogger {
+   public:
+    LifetimeLogger(int id) : id_(id) {
+        std::cout << "LifetimeLogger "s << id_ << " created." << std::endl;
+    }
+
+    ~LifetimeLogger() {
+        std::cout << "LifetimeLogger "s << id_ << " destroyed." << std::endl;
+    }
+
+   private:
+    int id_{};
+};
+
+int main() {
+    // создаем LifetimeLogger в куче, передавая его конструктору "5", в качестве параметра
+    LifetimeLogger* life_loggerPtr_1 = new LifetimeLogger(5);
+
+    // далее создадим LifetimeLogger в стеке
+    LifetimeLogger life_logger_2(111);
+
+    // Из вывода программы:
+    /*
+    LifetimeLogger 5 created.
+    LifetimeLogger 111 created.
+    LifetimeLogger 111 destroyed.
+     */
+    // видно, что память под объект "LifetimeLogger" выделенная в куче не была освобождена, поскольку программист забыл вызвать "delete".
+    // Разработаем класс ResourceManager, который позволит решить эту проблему
+
+    // теперь создаем LifetimeLogger, используя "ResourceManager" - память больше не утекает
+    ResourceManager<LifetimeLogger> life_logger_3(new LifetimeLogger(555));
+
+    return 0;
+}

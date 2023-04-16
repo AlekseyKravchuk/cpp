853131f9982a2cfbafbf42be14ac5bc8e02b4d81
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2023-04-16 23:32:19 +0300

    In the middle of solving personal_budget challenge

--- belts_cpp/02_yellow_belt/week_04/22_task_personal_budget.cpp
+++ belts_cpp/02_yellow_belt/week_04/22_task_personal_budget.cpp
@@ -1,75 +1,148 @@
 #include <cstdint>  // uint64_t
 #include <iostream>
 #include <limits>
+#include <set>
+#include <sstream>
 
 using namespace std::literals;
 
 class Date {
    public:
     Date() = default;
+
+    Date(const std::string& date) {
+        std::stringstream ss(date);
+        std::string year_str{}, month_str{}, day_str{};
+        if (ss && std::getline(ss, year_str, '-') && std::getline(ss, month_str, '-') && std::getline(ss, day_str)) {
+            _year = std::stoi(year_str);
+            _month = std::stoi(month_str);
+            _day = std::stoi(day_str);
+            _total_days = GetDaysFromEpoch();
+        } else {
+            throw std::runtime_error("Wrong date format. Correct format is: YYYY-MM-DD"s);
+        }
+    }
+
+    int GetYear() const { return _year; }
+    int GetMonth() const { return _month; }
+    int GetDay() const { return _day; }
+    int GetTotalDays() const { return _total_days; }
+
     void SetYear(int year) { _year = year; }
     void SetMonth(int month) { _month = month; }
     void SetDay(int day) { _day = day; }
+    void SetTotalDays(int total_days) { _total_days = total_days; }
+
+    int GetDaysFromEpoch() {
+        int year = _year - _year_start;
+        int days_count = 0;
+        days_count += year * 365;
+
+        // проверяем, нужно ли учитывать текущий год в подсчете вискокосных годов
+        if (_month < 2) {
+            --year;
+        }
+
+        days_count += daysUpToCurrentMonth[_month] + _day;
+        days_count += GetLeapYearsCount(year);
+        ++days_count;  // по условию задачи дипазон дат симметричный => добавляем единицу
+
+        return days_count;
+    }
 
    private:
-    int _year = 2000;
+    int _year = 1600;
     int _month = 1;
     int _day = 1;
-    uint64_t _total_days = 0;  // количество дней начиная с 01.01.2000
+    int _total_days = 0;  // количество дней начиная с "_year_start"
+    static const int _year_start = 1600;
+    static constexpr const int daysUpToCurrentMonth[13] = {0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+    // Определяет количество високосных годов в диапазоне [1600, year]
+    int GetLeapYearsCount(int year) {
+        // год является
+        return (year / 4) - (year / 100) + (year / 400);
+    }
 };
 
-// TODO:
-// https://www.geeksforgeeks.org/count-of-leap-years-in-a-given-year-range/ - Count of Leap Years in a given year range
-// Find number of days between two given dates - Find number of days between two given dates
+bool operator<(const Date& lhs, const Date& rhs) {
+    return lhs.GetTotalDays() < rhs.GetTotalDays();
+}
+
+class TInterval {
+   public:
+    int GetIntervalLength() const { return _duration_in_days; }
+    const Date& Start() const { return _start; }
+    const Date& End() const { return _end; }
+
+    void SetIntervalLength() {
+        // по условию задачи обе даты из диапазона [_start, _end] включены в диапазон => +1
+        _duration_in_days = _end.GetTotalDays() - _start.GetTotalDays() + 1;
+    }
+
+    friend std::istream& operator>>(std::istream& is, TInterval& interval);
+
+   private:
+    Date _start{};
+    Date _end{};
+    int _duration_in_days{};
+};
+
+bool operator<(const TInterval& lhs, const TInterval& rhs) {
+    // если интервалы не перекрываются слева
+    if (lhs.Start() < rhs.Start() && lhs.End() < rhs.Start()) {
+        return true;
+    } else {  // если интервалы перекрываются слева
+        return lhs.Start() < rhs.Start();
+    }
+}
+
 std::istream& operator>>(std::istream& is, Date& date) {
-    int day{0}, month{0}, year{0};
+    std::string day{}, month{}, year{};
 
-    is >> year;
-    if (is.peek() != '-') {
+    if (is && std::getline(is, year, '-') && std::getline(is, month, '-') && std::getline(is, day, ' ')) {
+        date.SetYear(std::stoi(year));
+        date.SetMonth(std::stoi(month));
+        date.SetDay(std::stoi(day));
+
+        int total_days = date.GetDaysFromEpoch();
+        date.SetTotalDays(total_days);
+    } else {
         throw std::runtime_error("Wrong date format. Correct format is: YYYY-MM-DD"s);
     }
-    is.ignore('-');
-
-    is >> month;
-    if (is.peek() != '-') {
-        throw std::runtime_error("Wrong date format. Correct format is: YYYY-MM-DD"s);
-    }
-
-    is >> day;
-
-    date.SetYear(year);
-    date.SetMonth(month);
-    date.SetDay(day);
-
-    // + 1 дополнительный элемент для удобства счета
-    const unsigned int daysUpToCurrentMonth[13] = {0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
 
     return is;
 }
 
-int GetDifferenceInDays(const Date& date_from, const Date& date_to) {
-}
+std::istream& operator>>(std::istream& is, TInterval& interval) {
+    if (is) {
+        is >> interval._start;
+        is >> interval._end;
+        interval.SetIntervalLength();
+    }
 
-// Returns: "true" если "year" является високосным годом, в противном случае - "false"
-// Определение високосного года:
-//     - год, номер которого кратен 400, — високосный;
-//     - остальные годы, номер которых кратен 100, — НЕвисокосные (например, годы 1700, 1800, 1900, 2100, 2200, 2300);
-//     - остальные годы, номер которых кратен 4, — високосные[5];
-//     - все остальные годы — невисокосные.
-constexpr bool is_leap(int year) noexcept {
-    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
-}
-
-int LeapYearsBefore(int year) {
-    year--;
-    return (year / 4) - (year / 100) + (year / 400);
+    return is;
 }
 
 int main() {
-    for (int year = 2000; year < 2100; ++year) {
-        if (is_leap(year)) {
-            std::cout << year << " "s;
-        }
-    }
+    // std::string date_str = "2000-04-01"s;
+    // std::string date_str = "2000-01-09"s;
+
+    std::string date_from_str = "2000-01-02"s;
+    std::string date_to_str = "2000-01-06"s;
+
+    std::stringstream oss("2000-01-02 2000-01-06 20"s);
+    TInterval ti_1;
+    int income;
+    oss >> ti_1 >> income;
+
+    std::set<TInterval> intervals;
+
+    // Date date_from(date_from_str);
+    // Date date_to(date_to_str);
+
+    // int delta = GetDaysDelta(date_from, date_to);
+    // std::cout << delta << std::endl;
+
     return 0;
 }

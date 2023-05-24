6c024190a6a2db949a15cfe2955b71768695782b
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2023-05-24 16:42:25 +0300

    red_belt: added solution to express_trains challenge

---
+++ belts_cpp/03_red_belt/week_02/13_task_express_trains/main.cpp
@@ -0,0 +1,131 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+// #define _GLIBCXX_DEBUG 1  // включить режим отладки
+
+#ifdef _GLIBCXX_DEBUG
+#include "input_redirection.h"
+#include "log_duration.h"
+#endif  //_GLIBCXX_DEBUG
+
+using namespace std::literals;
+
+/*
+================= Задание по программированию «Экспрессы» =================
+Вам даны задача и её решение — верное, но не удовлетворяющее заданным ограничениям на время работы. Переработайте это решение и сдайте в систему.
+Условие:
+Разработайте простейшую систему маршрутизации экспрессов, курсирующих по одному железнодорожному направлению,
+представляющему собой прямую. Ваша программа должна уметь обрабатывать запросы двух типов:
+    - ADD start finish — добавить в систему маршрутов экспресс, следующий со станции start до станции finish и обратно.
+      Экспресс не делает промежуточных остановок. Станции задаются целыми числами, равными их расстоянию от вокзала (он имеет номер 0).
+    - GO start finish — попытаться проложить беспересадочный маршрут от станции start до станции finish.
+      Если существует экспресс между этими двумя станциями, в ответ на данный запрос выведите 0.
+      В противном случае выведите положительное число — минимальное расстояние, на которое можно приблизиться к станции finish, стартовав строго на станции start и использовав не более одного экспресса.
+
+Формат входных данных:
+В первой строке вводится количество запросов Q — натуральное число, не превосходящее 10^5.
+В следующих Qстроках в соответствии с описанным выше форматом вводятся запросы.
+Гарантируется, что номера станций являются целыми числами, по модулю не превосходящими 10^9.
+
+Формат выходных данных:
+Для каждого запроса GO выведите единственное целое неотрицательное число — минимальное расстояние до конечной станции маршрута,
+вычисляемое в соответствии с описанными выше правилами.
+
+Ограничения:
+1 секунда на выполнение всех запросов.
+Все описанные в условии гарантии действительно справедливы для всех тестов, на которых будет запускаться ваша программа.
+Проверять корректность тестов не нужно.
+
+Пример
+Ввод:
+7
+ADD -2 5
+ADD 10 4
+ADD 5 8
+GO 4 10
+GO 4 -2
+GO 5 0
+GO 5 100
+
+Вывод:
+0
+6
+2
+92
+*/
+
+class RouteManager {
+   public:
+    void AddRoute(int start, int finish) {
+        _reachables[start].insert(finish);
+        _reachables[finish].insert(start);
+    }
+
+    int FindNearestFinish(int start, int finish) const {
+        int result = abs(start - finish);
+
+        if (!_reachables.count(start)) {
+            return result;
+        }
+
+        if (_reachables.at(start).count(finish)) {
+            return 0;
+        }
+
+        const std::set<int>& reachable_stations = _reachables.at(start);
+        if (!reachable_stations.empty()) {
+            auto it = reachable_stations.upper_bound(finish);
+
+            if (it == reachable_stations.begin()) {
+                result = std::min(result, abs(finish - *it));
+            } else if (it == reachable_stations.end()) {
+                result = std::min(result, abs(finish - *std::prev(it)));
+            } else {
+                result = std::min(result, std::min(std::abs(finish - *std::prev(it)), std::abs(finish - *it)));
+            }
+        }
+        return result;
+    }
+
+   private:
+    std::map<int, std::set<int>> _reachables;
+};
+
+void RunBody() {
+    RouteManager routes;
+    int query_count;
+    std::cin >> query_count;
+
+    for (int query_id = 0; query_id < query_count; ++query_id) {
+        std::string query_type;
+        std::cin >> query_type;
+        int start, finish;
+        std::cin >> start >> finish;
+        if (query_type == "ADD") {
+            routes.AddRoute(start, finish);
+        } else if (query_type == "GO") {
+            std::cout << routes.FindNearestFinish(start, finish) << "\n";
+        }
+    }
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+#ifdef _GLIBCXX_DEBUG
+    REDIRECT_INPUT("input.txt"s);
+    {
+        LOG_DURATION("RouteManager queries"s);
+        RunBody();
+    }
+#else
+    RunBody();
+#endif  //_GLIBCXX_DEBUG
+    return 0;
+}

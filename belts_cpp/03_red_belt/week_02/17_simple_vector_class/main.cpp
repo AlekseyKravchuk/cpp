92e1d1491cf22f5afe445b07d10f053b8895df2b
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2023-06-13 14:41:30 +0300

    red_belt: tiny changes

--- belts_cpp/03_red_belt/week_02/17_simple_vector_class/main.cpp
+++ belts_cpp/03_red_belt/week_02/17_simple_vector_class/main.cpp
@@ -1,7 +1,30 @@
 #include <algorithm>
 #include <iostream>
+#include <sstream>
 
 #include "simple_vector.h"
+
+using namespace std::literals;
+
+template <typename Collection>
+std::string Join(const Collection& collection, const std::string separator = " "s) {
+    std::ostringstream oss;
+    bool isFirst = true;
+    for (const auto& elm : collection) {
+        if (!isFirst) {
+            oss << separator;
+        }
+        isFirst = false;
+        oss << elm;
+    }
+
+    return oss.str();
+}
+
+template <typename T>
+void PrintVector(const SimpleVector<T>& v) {
+    std::cout << Join(v, ", "s) << std::endl;
+}
 
 int main() {
     SimpleVector<int> v(5);
@@ -9,6 +32,7 @@ int main() {
     // for (int i = 0; i < 5; ++i) {
     //     v[i] = 5 - i;
     // }
+
     v[0] = 55;
     v[1] = 20;
     v[2] = 33;
@@ -20,10 +44,12 @@ int main() {
     // }
     // std::cout << std::endl;
 
-    for (int elm : v) {
-        std::cout << elm << ' ';
-    }
-    std::cout << std::endl;
+    // // благодаря тому, что в классе "SimpleVector" реализованы методы "begin" и "end", можем использовать "range-based" цикл "for"
+    // for (int elm : v) {
+    //     std::cout << elm << ' ';
+    // }
+    // std::cout << std::endl;
+    PrintVector(v);
 
     // благодаря тому, что мы дали нашему вектору итераторы (методы "begin" и "end"), мы можем использовать его в алгоритмах
     std::sort(v.begin(), v.end());
--- belts_cpp/03_red_belt/week_02/17_simple_vector_class/simple_vector.h
+++ belts_cpp/03_red_belt/week_02/17_simple_vector_class/simple_vector.h
@@ -8,32 +8,20 @@ class SimpleVector {
     explicit SimpleVector(size_t size) : _size(size),
                                          _data(new T[size]) {}
 
-    ~SimpleVector() {
-        delete[] _data;
-    }
+    ~SimpleVector() { delete[] _data; }
 
     // оператор доступа к элементу вектора по индексу (оператор индексирования, subscript operator)
-    T& operator[](size_t index) {
-        return _data[index];
-    }
+    T& operator[](size_t index) { return _data[index]; }
 
     // в качестве возвращаемого значения можно использовать УКАЗАТЕЛЬ на первый элемент нашего вектора
-    T* begin() {
-        return _data;
-    }
+    T* begin() { return _data; }
+    const T* begin() const { return _data; }
 
     // в качестве возвращаемого значения можно использовать УКАЗАТЕЛЬ на элемент ЗА ПОСЛЕДНИМ (ont-past-the-end)
-    T* end() {
-        return _data + _size;
-    }
+    T* end() { return _data + _size; }
+    const T* end() const { return _data + _size; }
 
    private:
     size_t _size;
     T* _data;
 };
-
-// // оператор доступа к элементу вектора по индексу
-// // template <typename T>
-// T& operator[](size_t index) {
-//     return *(_data + index);
-// }

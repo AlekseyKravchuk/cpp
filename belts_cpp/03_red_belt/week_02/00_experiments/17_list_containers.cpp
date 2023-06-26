61a1635b15bb3874d7fcfbe83bb13e1b0a582e6a
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2023-06-26 14:49:58 +0300

    red_belt: tiny changes

---
+++ belts_cpp/03_red_belt/week_02/00_experiments/17_list_containers.cpp
@@ -0,0 +1,9 @@
+#include <list>
+#include <string>
+
+int main() {
+    // std::list<std::string> words;
+    std::list<std::string> sayings{20};  // A list of 20 empty strings
+    
+    return 0;
+}

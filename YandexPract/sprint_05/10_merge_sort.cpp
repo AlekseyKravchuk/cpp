7bb757282cb0a4a5a390a491474377680c428a1a
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2023-08-28 16:00:11 +0300

    red_belt: tiny changes

--- YandexPract/sprint_05/10_merge_sort.cpp
+++ YandexPract/sprint_05/10_merge_sort.cpp
@@ -11,7 +11,6 @@ using namespace std;
 // функция, записывающая элементы диапазона в строку
 template <typename It>
 string PrintRangeToString(It range_begin, It range_end) {
-    // удобный тип ostringstream -> https://ru.cppreference.com/w/cpp/io/basic_ostringstream
     ostringstream out;
     for (auto it = range_begin; it != range_end; ++it) {
         out << *it << " "s;
--- YandexPract/sprint_08/01_debug_macroses.cpp
+++ YandexPract/sprint_08/01_debug_macroses.cpp
@@ -25,7 +25,8 @@ void PrintRangeComma(It range_begin, It range_end) {
 template <typename RandomIt>
 void MergeSort(RandomIt range_begin, RandomIt range_end) {
     // 1. Если диапазон содержит меньше 2 элементов, выходим из функции
-    int range_length = std::distance(range_begin, range_end);
+    // int range_length = std::distance(range_begin, range_end);
+    int range_length = range_end - range_begin;
     if (range_length < 2) {
         return;
     }
@@ -33,17 +34,15 @@ void MergeSort(RandomIt range_begin, RandomIt range_end) {
     // 2. Создаём вектор, содержащий все элементы текущего диапазона
     vector<typename RandomIt::value_type> elements(range_begin, range_end);
 
-    // 3. Разбиваем вектор на две равные части
-    auto mid = elements.begin() + range_length / 2;
+    // 3. Разбиваем вектор на две равные части: вычисляем значение итератора, указывающего приблизительно на срединный элемент
+    auto it_mid = elements.begin() + range_length / 2;
 
     // 4. Вызываем функцию MergeSort от каждой половины вектора
-    MergeSort(elements.begin(), mid);
-    MergeSort(mid, elements.end());
+    MergeSort(elements.begin(), it_mid);
+    MergeSort(it_mid, elements.end());
 
-    // 5. С помощью алгоритма merge сливаем отсортированные половины
-    // в исходный диапазон
-    // merge -> http://ru.cppreference.com/w/cpp/algorithm/merge
-    merge(elements.begin(), mid, mid, elements.end(), range_begin);
+    // 5. С помощью алгоритма "std::merge" сливаем отсортированные половины в исходный диапазон
+    merge(elements.begin(), it_mid, it_mid, elements.end(), range_begin);
 }
 
 void TestFromYandex() {

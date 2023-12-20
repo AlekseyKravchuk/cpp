5903b3aa86301953aec762111e0a258b1d14f23c
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2023-12-20 22:01:53 +0300

    brown_belt: tiny changes

---
+++ belts_cpp/04_brown_belt/01_week/07_personal_budget_control/xml.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <istream>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
+
+namespace Xml {
+  
+class Node {
+   public:
+    Node(std::string name, std::unordered_map<std::string, std::string> attrs);
+
+    const std::vector<Node>& Children() const;
+
+    void AddChild(Node node);
+
+    std::string_view Name() const;
+
+    template <typename T>
+    T AttributeValue(const std::string& name) const;
+
+   private:
+    std::string _name;
+    std::vector<Node> _children;
+    std::unordered_map<std::string, std::string> _attrs;
+};
+
+class Document {
+   public:
+    explicit Document(Node root);
+
+    const Node& GetRoot() const;
+
+   private:
+    Node _root;
+};
+}  // namespace Xml
+
+Xml::Document Load(std::istream& input);
+
+template <typename T>
+inline T Xml::Node::AttributeValue(const std::string& name) const {
+    std::istringstream attr_input(attrs.at(name));
+    T result;
+    attr_input >> result;
+
+    return result;
+}

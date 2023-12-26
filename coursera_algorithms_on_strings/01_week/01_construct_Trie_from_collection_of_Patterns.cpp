f1df1b508ae83789626b0244242a429433a23ed3
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2023-12-26 23:39:01 +0300

    coursera_string_algs: week_1 challenge_1 in progress

--- coursera_algorithms_on_strings/01_week/01_construct_Trie_from_collection_of_Patterns.cpp
+++ coursera_algorithms_on_strings/01_week/01_construct_Trie_from_collection_of_Patterns.cpp
@@ -1,7 +1,11 @@
+#include <fstream>
 #include <iostream>
+#include <limits>  // std::numeric_limits
 #include <map>
 #include <string>
 #include <vector>
+
+#define _GLIBCXX_DEBUG 1  // включить режим отладки
 
 using std::map;
 using std::string;
@@ -10,14 +14,46 @@ using std::vector;
 using edges = map<char, int>;
 using trie = vector<edges>;
 
+// TODO:
 trie build_trie(vector<string>& patterns) {
-    trie t;
-    // write your code here
-    return t;
+    uint MAX_PATTERN_LENGTH = 101;
+    trie pref_tree(MAX_PATTERN_LENGTH);
+    uint node_cntr = 0;
+
+    for (const auto& pattern : patterns) {
+        uint src = 0;
+        uint dst = 0;
+
+        for (auto ch : pattern) {
+            if (pref_tree[src].count(ch)) {
+                src = pref_tree[src].at(ch);
+                continue;
+            } else {
+                dst = node_cntr + 1;
+                pref_tree[src].emplace(ch, dst);
+                src = dst;
+                ++node_cntr;
+            }
+        }
+    }
+
+    return pref_tree;
 }
 
 int main() {
     size_t n;
+
+#ifdef _GLIBCXX_DEBUG
+    // string fname = "input_2.txt";
+    string fname = "input_3.txt";
+    std::ifstream input(fname);  // configuring input from the file
+    if (!input) {
+        std::cerr << "File \"" << fname << "\" is not opened. Exiting." << std::endl;
+    }
+    std::streambuf* cinbuf = std::cin.rdbuf();  // save old buf
+    std::cin.rdbuf(input.rdbuf());              // redirect std::cin to "input" file stream
+#endif                                          // _GLIBCXX_DEBUG
+
     std::cin >> n;
     vector<string> patterns;
 
@@ -28,12 +64,16 @@ int main() {
     }
 
     trie t = build_trie(patterns);
-    
+
     for (size_t i = 0; i < t.size(); ++i) {
         for (const auto& j : t[i]) {
             std::cout << i << "->" << j.second << ":" << j.first << "\n";
         }
     }
+
+#ifdef _GLIBCXX_DEBUG
+    std::cin.rdbuf(cinbuf);  // reset to standard input again
+#endif
 
     return 0;
 }

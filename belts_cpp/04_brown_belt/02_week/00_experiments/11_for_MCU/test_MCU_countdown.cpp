5518817f912d96efb5620919ed4b466f8cd2881a
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2024-01-24 13:28:59 +0300

    tiny changes, added some tests for MCU program (lesson_12)

--- belts_cpp/04_brown_belt/02_week/00_experiments/11_for_MCU/test_MCU_countdown.cpp
+++ belts_cpp/04_brown_belt/02_week/00_experiments/11_for_MCU/test_MCU_countdown.cpp
@@ -109,6 +109,68 @@ void Test_GetIndexOfFirstNonZeroDigit_All_Zeros() {
     ASSERT_EQUAL(first_digit_expected, digits[start_index]);
 }
 
+void Test_DigitsFromNumber_4digits() {
+    ResetDigits(digits);
+
+    int number = 2469;
+    GetDigitsFromNumber(number, digits, 4);
+
+    int digits_expected[4] = {2, 4, 6, 9};
+    for (size_t i = 0; i < 4; ++i) {
+        ASSERT_EQUAL(digits[i], digits_expected[i]);
+    }
+}
+
+void Test_DigitsFromNumber_3digits() {
+    ResetDigits(digits);
+
+    int number = 469;
+    GetDigitsFromNumber(number, digits, 4);
+
+    int digits_expected[4] = {0, 4, 6, 9};
+    for (size_t i = 0; i < 4; ++i) {
+        ASSERT_EQUAL(digits[i], digits_expected[i]);
+    }
+}
+
+void Test_DigitsFromNumber_2digits() {
+    ResetDigits(digits);
+
+    int number = 69;
+    GetDigitsFromNumber(number, digits, 4);
+
+    int digits_expected[4] = {0, 0, 6, 9};
+    for (size_t i = 0; i < 4; ++i) {
+        ASSERT_EQUAL(digits[i], digits_expected[i]);
+    }
+}
+
+void Test_DigitsFromNumber_1digits() {
+    ResetDigits(digits);
+
+    int number = 9;
+    GetDigitsFromNumber(number, digits, 4);
+
+    int digits_expected[4] = {0, 0, 0, 9};
+    for (size_t i = 0; i < 4; ++i) {
+        ASSERT_EQUAL(digits[i], digits_expected[i]);
+    }
+}
+
+void Test_DigitsFromNumber_all_zeros() {
+    ResetDigits(digits);
+
+    int number = 0;
+    GetDigitsFromNumber(number, digits, 4);
+
+    int digits_expected[4] = {0, 0, 0, 0};
+    for (size_t i = 0; i < 4; ++i) {
+        ASSERT_EQUAL(digits[i], digits_expected[i]);
+    }
+}
+
+
+
 int main() {
     TestRunner tr;
     RUN_TEST(tr, Test_GetIndexOfFirstNonZeroDigit_4digits);
@@ -116,6 +178,14 @@ int main() {
 	RUN_TEST(tr, Test_GetIndexOfFirstNonZeroDigit_2digits);
 	RUN_TEST(tr, Test_GetIndexOfFirstNonZeroDigit_1digit);
 	RUN_TEST(tr, Test_GetIndexOfFirstNonZeroDigit_All_Zeros);
+
+    // =====================================================
+    RUN_TEST(tr, Test_DigitsFromNumber_4digits);
+    RUN_TEST(tr, Test_DigitsFromNumber_3digits);
+    RUN_TEST(tr, Test_DigitsFromNumber_2digits);
+    RUN_TEST(tr, Test_DigitsFromNumber_1digits);
+    RUN_TEST(tr, Test_DigitsFromNumber_all_zeros);
+    // =====================================================
 	
     // int number = 7965;
     // GetDigitsFromNumber(number, digits, 4);

25dbd55171faf8fbe79f2fc0c3d2522f227e6f25
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2024-01-31 13:56:39 +0300

    AVR_MCU: added code for forward counting and countdown on 4-digit 7-segment ingicator

--- AVR_MCU_programming/12_02_4digit_7seg_countdown_via_clear_timer_on_compare_match_mode_CTC/4digit_7seg_countdown_via_clear_timer_on_compare_match_mode_CTC.c
+++ AVR_MCU_programming/12_02_4digit_7seg_countdown_via_clear_timer_on_compare_match_mode_CTC/4digit_7seg_countdown_via_clear_timer_on_compare_match_mode_CTC.c
@@ -1,3 +1,4 @@
+#include <avr/io.h>
 #define F_CPU 1000000UL  // set MK frequency to 1MHz
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -7,13 +8,13 @@
 #include <stddef.h>  // size_t
 
 void TurnOnDigit(int index_of_common_cathode_pin);
-void GetDigitsFromNumber(int number);
+int GetDigitsFromNumber(int number);
 
-// use port D for segments control (7 segments: a, b, c, d, e, f, g)
+// use port D to control all of 7 segments: a, b, c, d, e, f, g
 #define SEG7_DDRx  DDRD
 #define SEG7_PORTx PORTD
 
-// use port B for common cathodes control (4 common cathodes in case of 4-digit 7-segment indicator)
+// use port B to control 4 common cathodes in case of 4-digit 7-segment indicator
 #define CC_DDRx DDRB
 #define CC_PORTx PORTB
 
@@ -28,59 +29,72 @@ int digits_7seg_CC[] = {  // PORTX values to highlight digits in 7-segment indic
 	0b00000111,  // highlight "7"  Common Cathode (CC)
 	0b01111111,  // highlight "8"  Common Cathode (CC)
 	0b01101111,  // highlight "9"  Common Cathode (CC)
-	0b10000000,  // highlight "dp/colon" Common Cathode (CC)
+	0b10000000   // highlight "dp/colon" Common Cathode (CC)
 };
 
 // ================== Global variables ==================
-int cc_pins[4] = {0, 1, 2, 3};  // set common cathodes pins for selected port (PB0, PB1, PB2, PB3)
-int cc_idx = 0;  // index of currently active common cathode (GROUND will be applied to that pin): [0,1,2,3]
+int cc_pins[4] = {0, 1, 2, 3};  // common cathodes pins for selected port (PB0, PB1, PB2, PB3)
+int cc_idx = 0;  // index of currently active common cathode (GROUND will be applied to that pin: [0,1,2,3])
 
 int digits_of_number[] = {0, 0, 0, 0};
-const size_t d_length = sizeof(digits_of_number) / sizeof(int); // len == 4
-	
-int seconds_cntr = 0;
+const size_t d_length = sizeof(digits_of_number) / sizeof(int); // length == 4
+
+int COUNTER_MIN_VAL = 31;
+int seconds_counter = 38;
+int start_pos = 0; // position of first nonzero digit in number (i.e. skipping leading zeros in number)
 
 // =======================================================
 
-//Interrupt Service Routine will be called each 2 milliseconds for each digit, so period T = 2ms*4digits = 8ms
+//interrupt handler will be called each 2 milliseconds for each digit, so period T = 2ms*4digits = 8ms
 ISR(TIMER0_OVF_vect) {
+	if (cc_idx < start_pos) {
+		cc_idx = start_pos;
+	}
+
 	TurnOnDigit(cc_idx);
 	cc_idx = (cc_idx + 1) % 4;
 }
 
-// this interrupt will be triggered each 1 second
+// interrupt handler will be called each 1 second
 ISR(TIMER1_COMPA_vect) {
-	//seconds_cntr = (seconds_cntr + 1) % 10000;
-	//seconds_cntr = (seconds_cntr > 9999) ? 0 : seconds_cntr + 1;
-	if (++seconds_cntr > 5) {
-		seconds_cntr = 0;
-		PORTC |= 1<<0;
+	if (seconds_counter > COUNTER_MIN_VAL) {
+		--seconds_counter;
 	}
-	
-	GetDigitsFromNumber(seconds_cntr);
-	//PORTC ^= 1<<0;  // toggle LED connected to PC0
+
+	start_pos = GetDigitsFromNumber(seconds_counter);
+	PORTC ^= 1<<0;  // toggle LED connected to PC0
 }
 
-void GetDigitsFromNumber(int number) {
-	//if (!number) { return; }
+// returns the position of the first non-zero digit (or pos = d_length-1 if number == 0) in a "d_length"-digit number
+int GetDigitsFromNumber(int number) {
+	if (number == 0) {
+		digits_of_number[d_length - 1] = 0;
+		return d_length - 1;
+	}
 
-	int i = d_length - 1;  // initial value of i == 3
+	int i = d_length - 1; // initial value of i == 3
+	int pos = i;
+
 	while (number) {
 		digits_of_number[i--] = number % 10;
 		number /= 10;
+
+		if (number) {
+			--pos;
+		}
 	}
+
+	return pos;
 }
 
-void TurnOnDigit(int cc_pin_index) {  // cc_pos: [0, 1, 2, 3]
-	// Say our number is 1579, so digits_of_number = [1, 5, 7, 9]
-	// At a given time we need to TURN ON 
-	// we need to ensure that all common cathodes are DISABLED (i.e. supplied with HIGH potential)
+void TurnOnDigit(int cc_pin_index) {
+	// supplied all common cathodes with HIGH potential, i.e. disable all digit on 4_digit_7seg_indicator
 	CC_PORTx |= (1 << cc_pins[0]) | (1 << cc_pins[1])   | (1 << cc_pins[2])  | (1 << cc_pins[3]);
-	
-	// select  which should be turned ON (configure needed segments)
+
+	// cc_pin_index specifies which position in number should be turned on
 	int digit = digits_of_number[cc_pin_index];
 	SEG7_PORTx = digits_7seg_CC[digit];
-	
+
 	// apply GROUND to desired common cathode pin (set '0' in corresponding bit in PORTx)
 	CC_PORTx &= ~(1<<cc_pins[cc_pin_index]);
 }
@@ -100,52 +114,48 @@ void ConfigureCommonCathodesPins() {
 }
 
 void ConfigureStandaloneLED() {
-	DDRC |= 1<<0;      // set PC0 as OUTPUT
-	PORTC &= ~(1<<0);  // set PC0 as LOW level
+	DDRC |= 1<<0 | 1<<0;      // set PC0 and PC1 as OUTPUT
+	PORTC &= ~(1<<0 | 1<<1);  // set PC0 and PC1 as LOW level
 }
 
-void ConfigureTimerCounter() {
-	//// ====================== Configure Timer/Counter0 =====================
+void ConfigureTimerCounters() {
+	//// ====================== Configure Timer/Counter TCNT0 =====================
 	// Timer/Counter0 Control Register: enable running TCNT0 and set prescaler = clk/8
-	// Toverflow = prescaler * max_TCNT_val / freq_MCU = 8 * 255 / 10**6 = 0.00204 seconds ~ 2 miliseconds
-	TCCR0 |= 1<<CS01;    
-	TCCR0 &= ~(1<<CS00); 
-	
-	TCNT0 = 0;
+	// T_overflow = prescaler * max_TCNT0_val / freq_MCU = 8 * 255 / 10**6 = 0.00204 seconds ~ 2 miliseconds
+	TCCR0 |= 1<<CS01;
+	TCCR0 &= ~(1<<CS00);
+
+	TCNT0 = 0;          // initialize counter with 0
+
 	TIMSK |= 1<<TOIE0;  // Bit 0 – TOIE0: Timer/Counter0 Overflow Interrupt Enable
-	//// ================ END of Timer/Counter0 configuration ================
-	
-	// ====================== Configure Timer/Counter1 =====================
-	// set Prescaler (clk/64) for Timer/Counter1; 1'000'000 / 64 = 15'625 Hz ()
+	//// ================ END of Timer/Counter TCNT0 configuration ================
+
+	// ======================= Configure Timer/Counter TCNT1 ======================
+	// set Prescaler = clk/64 for Timer/Counter1; 1'000'000 / 64 = 15'625 Hz
 	TCCR1B |= 1<<CS11 | 1<<CS10;
 	TCCR1B &= ~(1<<CS12);
-	
+
 	TCNT1 = 0;
 	OCR1A = 15625;       // Output Compare Register, when TCNT1 becomes equal to OCR1AL, interrupt will occur
 	TIMSK |= 1<<OCIE1A;  // Bit 4 – OCIE1A: Timer/Counter1, Output Compare A Match Interrupt Enable
 	TCCR1B |= 1<<WGM12;  // reset TCNT1 immediately when match occured; mode #4: Clear Timer on Compare Match (CTC)
-	
-	// ================ END of Timer/Counter1 configuration ================
-	
-	sei();  // Status Register Bit 7 – I: Global Interrupt Enable
+	// ================ END of Timer/Counter TCNT1 configuration ================
 }
-
-
 
 int main() {
 	ConfigureSegmentsPins();
 	ConfigureCommonCathodesPins();
-	//ConfigureTimerCounter();
+	ConfigureTimerCounters();
 	ConfigureStandaloneLED();
 
-	int number = 1297;
+	start_pos = GetDigitsFromNumber(seconds_counter);
+
+	sei();  // Enable Interrupts Globally: Status Register Bit 7 – I: Global Interrupt Enable
+
 	while (true) {
 		// all useful stuff is implemented in Interrupt Service Routines (ISR), or Interrupt Handlers
-		GetDigitsFromNumber(number);
-		for (size_t i = 0; i < d_length; ++i) {
-			TurnOnDigit(i);
-		}
 	}
 
 	return 0;
 }
+
--- AVR_MCU_programming/12_03_debugging_GetDigitsFromNumber/debugging_GetDigitsFromNumber.cpp
+++ AVR_MCU_programming/12_03_debugging_GetDigitsFromNumber/debugging_GetDigitsFromNumber.cpp
@@ -5,181 +5,185 @@
 
 using namespace std;
 
-int digits[4] = {0, 0, 0, 0};
+int digits_of_number[4] = {0, 0, 0, 0};
+const size_t d_length = sizeof(digits_of_number) / sizeof(int); // length == 4
+
 int start = 0;
+int seconds_counter = 0;
 
-void ResetDigits(int* digits);
-
-void GetDigitsFromNumber(int number, int* digits, const int digits_len) {
-    assert(digits_len == 4);
-
-    if (!number) {
-        return;
-    }
-
-    int i = 3;
-    while (number) {
-        digits[i--] = number % 10;
-        number /= 10;
+void ResetDigits() {
+    for (size_t i = 0; i < d_length; ++i) {
+        digits_of_number[i] = 0;
     }
 }
 
-int GetIndexOfFirstNonZeroDigit(int* digits) {
-    int i = 0;
-    if (digits[i] == 0) {
-        ++i;
-        while (!digits[i] && i != 3) {
-            ++i;
-        }
-    }
+// returns the position of the first non-zero digit (or pos = d_length-1 if number == 0) in a "d_length"-digit number
+int GetDigitsFromNumber(int number) {
+	if (number == 0) {
+		digits_of_number[d_length - 1] = 0;
+		return d_length - 1;
+	}
 
-    return i;
-}
+	int i = d_length - 1; // initial value of i == 3
+	int pos = i;
 
-void ResetDigits(int* digits) {
-    for (int i = 0; i < 4; ++i) {
-        digits[i] = 0;
-    }
+	while (number) {
+		digits_of_number[i--] = number % 10;
+		number /= 10;
+
+		if (number) {
+			--pos;
+		}
+	}
+
+	return pos;
 }
 
 void Test_GetIndexOfFirstNonZeroDigit_4digits() {
     {
-        ResetDigits(digits);
+        ResetDigits();
         int number = 7965;
         int start_index_expected = 0;
         int first_digit_expected = 7;
 
-        GetDigitsFromNumber(number, digits, 4);
-        int start_index = GetIndexOfFirstNonZeroDigit(digits);
+        int start_index = GetDigitsFromNumber(number);
 
         ASSERT_EQUAL(start_index_expected, start_index);
-        ASSERT_EQUAL(first_digit_expected, digits[start_index]);
+        ASSERT_EQUAL(first_digit_expected, digits_of_number[start_index]);
     }
 }
 
 void Test_GetIndexOfFirstNonZeroDigit_3digits() {
-    ResetDigits(digits);
+    ResetDigits();
     int number = 965;
     int start_index_expected = 1;
     int first_digit_expected = 9;
 
-    GetDigitsFromNumber(number, digits, 4);
-    int start_index = GetIndexOfFirstNonZeroDigit(digits);
+    int start_index = GetDigitsFromNumber(number);
 
     ASSERT_EQUAL(start_index_expected, start_index);
-    ASSERT_EQUAL(first_digit_expected, digits[start_index]);
+    ASSERT_EQUAL(first_digit_expected, digits_of_number[start_index]);
 }
 
 void Test_GetIndexOfFirstNonZeroDigit_2digits() {
-    ResetDigits(digits);
+    ResetDigits();
     int number = 65;
     int start_index_expected = 2;
     int first_digit_expected = 6;
 
-    GetDigitsFromNumber(number, digits, 4);
-    int start_index = GetIndexOfFirstNonZeroDigit(digits);
+    int start_index = GetDigitsFromNumber(number);
 
     ASSERT_EQUAL(start_index_expected, start_index);
-    ASSERT_EQUAL(first_digit_expected, digits[start_index]);
+    ASSERT_EQUAL(first_digit_expected, digits_of_number[start_index]);
+}
+
+void Test_GetIndexOfFirstNonZeroDigit_2digits_2() {
+    ResetDigits();
+    int number = 28;
+    int start_index_expected = 2;
+    int first_digit_expected = 2;
+
+    int start_index = GetDigitsFromNumber(number);
+
+    ASSERT_EQUAL(start_index_expected, start_index);
+    ASSERT_EQUAL(first_digit_expected, digits_of_number[start_index]);
 }
 
 void Test_GetIndexOfFirstNonZeroDigit_1digit() {
-    ResetDigits(digits);
+    ResetDigits();
     int number = 5;
     int start_index_expected = 3;
     int first_digit_expected = 5;
 
-    GetDigitsFromNumber(number, digits, 4);
-    int start_index = GetIndexOfFirstNonZeroDigit(digits);
+    int start_index = GetDigitsFromNumber(number);
 
     ASSERT_EQUAL(start_index_expected, start_index);
-    ASSERT_EQUAL(first_digit_expected, digits[start_index]);
+    ASSERT_EQUAL(first_digit_expected, digits_of_number[start_index]);
 }
 
 void Test_GetIndexOfFirstNonZeroDigit_All_Zeros() {
-    ResetDigits(digits);
+    ResetDigits();
     int number = 0;
     int start_index_expected = 3;
     int first_digit_expected = 0;
 
-    GetDigitsFromNumber(number, digits, 4);
-    int start_index = GetIndexOfFirstNonZeroDigit(digits);
+    int start_index = GetDigitsFromNumber(number);
 
     ASSERT_EQUAL(start_index_expected, start_index);
-    ASSERT_EQUAL(first_digit_expected, digits[start_index]);
+    ASSERT_EQUAL(first_digit_expected, digits_of_number[start_index]);
 }
 
 void Test_DigitsFromNumber_4digits() {
-    ResetDigits(digits);
+    ResetDigits();
 
     int number = 2469;
-    GetDigitsFromNumber(number, digits, 4);
+    GetDigitsFromNumber(number);
 
     int digits_expected[4] = {2, 4, 6, 9};
     for (size_t i = 0; i < 4; ++i) {
-        ASSERT_EQUAL(digits[i], digits_expected[i]);
+        ASSERT_EQUAL(digits_of_number[i], digits_expected[i]);
     }
 }
 
 void Test_DigitsFromNumber_3digits() {
-    ResetDigits(digits);
+    ResetDigits();
 
     int number = 469;
-    GetDigitsFromNumber(number, digits, 4);
+    GetDigitsFromNumber(number);
 
     int digits_expected[4] = {0, 4, 6, 9};
     for (size_t i = 0; i < 4; ++i) {
-        ASSERT_EQUAL(digits[i], digits_expected[i]);
+        ASSERT_EQUAL(digits_of_number[i], digits_expected[i]);
     }
 }
 
 void Test_DigitsFromNumber_2digits() {
-    ResetDigits(digits);
+    ResetDigits();
 
     int number = 69;
-    GetDigitsFromNumber(number, digits, 4);
+    GetDigitsFromNumber(number);
 
     int digits_expected[4] = {0, 0, 6, 9};
     for (size_t i = 0; i < 4; ++i) {
-        ASSERT_EQUAL(digits[i], digits_expected[i]);
+        ASSERT_EQUAL(digits_of_number[i], digits_expected[i]);
     }
 }
 
 void Test_DigitsFromNumber_1digits() {
     {
-        ResetDigits(digits);
+        ResetDigits();
 
         int number = 9;
-        GetDigitsFromNumber(number, digits, 4);
+        GetDigitsFromNumber(number);
 
         int digits_expected[4] = {0, 0, 0, 9};
         for (size_t i = 0; i < 4; ++i) {
-            ASSERT_EQUAL(digits[i], digits_expected[i]);
+            ASSERT_EQUAL(digits_of_number[i], digits_expected[i]);
         }
     }
 
     {
-        ResetDigits(digits);
+        ResetDigits();
 
         int number = 1;
-        GetDigitsFromNumber(number, digits, 4);
+        GetDigitsFromNumber(number);
 
         int digits_expected[4] = {0, 0, 0, 1};
         for (size_t i = 0; i < 4; ++i) {
-            ASSERT_EQUAL(digits[i], digits_expected[i]);
+            ASSERT_EQUAL(digits_of_number[i], digits_expected[i]);
         }
     }
 }
 
 void Test_DigitsFromNumber_all_zeros() {
-    ResetDigits(digits);
+    ResetDigits();
 
     int number = 0;
-    GetDigitsFromNumber(number, digits, 4);
+    GetDigitsFromNumber(number);
 
     int digits_expected[4] = {0, 0, 0, 0};
     for (size_t i = 0; i < 4; ++i) {
-        ASSERT_EQUAL(digits[i], digits_expected[i]);
+        ASSERT_EQUAL(digits_of_number[i], digits_expected[i]);
     }
 }
 
@@ -197,6 +201,7 @@ int main() {
     RUN_TEST(tr, Test_DigitsFromNumber_2digits);
     RUN_TEST(tr, Test_DigitsFromNumber_1digits);
     RUN_TEST(tr, Test_DigitsFromNumber_all_zeros);
+    RUN_TEST(tr, Test_GetIndexOfFirstNonZeroDigit_2digits_2);
     // =====================================================
 
     // int number = 7965;

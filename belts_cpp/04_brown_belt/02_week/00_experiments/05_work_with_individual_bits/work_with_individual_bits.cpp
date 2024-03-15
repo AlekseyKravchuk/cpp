84d7d7b427412366e40d98e27ecf564bc8ef7500
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2024-03-15 18:24:08 +0300

    tiny changes: work with individual bit, bitset

--- belts_cpp/04_brown_belt/02_week/00_experiments/05_work_with_individual_bits/work_with_individual_bits.cpp
+++ belts_cpp/04_brown_belt/02_week/00_experiments/05_work_with_individual_bits/work_with_individual_bits.cpp
@@ -1,12 +1,21 @@
 #include <bitset>
 #include <iostream>
 #include <iomanip>
+#include <bit>
 
-using namespace std;;
+using namespace std;
 
 int main() {
-    cout << setw(2) << std::left << "x" << " = "  << bitset<8>(1<<6) << endl;
-    cout << setw(2) << std::left << "~x" << " = " << bitset<8>(~(1<<6)) << endl;
-    
+    cout << setw(2) << std::left << "x" << " = " << bitset<8>(1u << 6) << endl;
+    cout << setw(2) << std::left << "~x" << " = " << bitset<8>(~(1u << 6)) << endl;
+
+     bitset<8> bs(4);
+    cout << "number of ones in " << bs << " = " << bs.count() << endl;
+
+    // TODO: найти способ быстро находить индекс первой 1 в битовом представлении числа
+//    cout << "index of first 1 in " << bs << "(bitset<8>)" << " = " << __builtin_ffs(uint8_t (bs)) << endl;
+    // cout << "number of ones in " << bitset<8>(37) << " = " << bitset<8>(37).count() << endl;
+
+
     return 0;
 }

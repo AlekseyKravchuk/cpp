0f4026637eebcc3fe743890ebe5851bc2e814a4e
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2024-03-17 22:13:00 +0300

    tiny changes, bitwise operations

--- belts_cpp/04_brown_belt/02_week/00_experiments/05_work_with_individual_bits/work_with_individual_bits.cpp
+++ belts_cpp/04_brown_belt/02_week/00_experiments/05_work_with_individual_bits/work_with_individual_bits.cpp
@@ -13,6 +13,14 @@ int main() {
     cout << "number of ones in " << bs << " = " << bs.count() << endl;
 
     // TODO: найти способ быстро находить индекс первой 1 в битовом представлении числа
+    // Пусть дано некоторое число N.
+    // Требуется найти индекс первого установленного бита в двоичном представлении этого числа.
+    // const uint32_t N = 9;  // 0b1001
+    // const uint32_t N = 2;  // 0b10
+    // const uint32_t N = 8;  // 0b1000
+    const uint32_t N = 16;  // 0b10000
+    // cout << "index of first 1 in " << bitset<8>(N) << " = " << __builtin_ffs(N) << endl;
+    cout << "index of first 1 in " << bitset<8>(N) << " = " << __lg(N) << endl;
 //    cout << "index of first 1 in " << bs << "(bitset<8>)" << " = " << __builtin_ffs(uint8_t (bs)) << endl;
     // cout << "number of ones in " << bitset<8>(37) << " = " << bitset<8>(37).count() << endl;
 

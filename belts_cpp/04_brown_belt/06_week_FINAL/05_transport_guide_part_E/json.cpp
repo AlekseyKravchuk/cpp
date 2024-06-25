cfc6209256bff60465c4365d65e25571f235b415
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2024-06-25 07:15:46 +0300

    brown_belt: final challenge, started transport guide, part E.

---
+++ belts_cpp/04_brown_belt/06_week_FINAL/05_transport_guide_part_E/json.cpp
@@ -0,0 +1,113 @@
+#include "json.h"
+
+#include <utility>
+
+using namespace std;
+
+namespace Json {
+
+    Document::Document(Node root)
+            : root(std::move(root)) { }
+
+    const Node& Document::GetRoot() const {
+        return root;
+    }
+
+    Node LoadNode(istream& input);  // forward declaration
+
+    Node LoadArray(istream& input) {
+        vector<Node> result;
+
+        for (char c; input >> c && c != ']';) {
+            if (c != ',') {
+                input.putback(c);
+            }
+            result.push_back(LoadNode(input));
+        }
+
+        return Node{std::move(result)};
+    }
+
+    Node LoadString(istream& input) {
+        string line;
+        getline(input, line, '"');
+        return Node{std::move(line)};
+    }
+
+    Node LoadDict(istream& input) {
+        map<string, Node> result;
+
+        for (char c; input >> c && c != '}';) {
+            if (c == ',') {
+                input >> c;
+            }
+
+            string key = LoadString(input).AsString();
+            input >> c;
+            result.emplace(std::move(key), LoadNode(input));
+        }
+
+        return Node{std::move(result)};
+    }
+
+    Node LoadVal(istream& input) {
+        string str;
+        char c;
+        input >> c;
+        while (isdigit(c) || c == '-' || c == '.') {
+            str += c;
+            input >> c;
+        }
+        input.putback(c);
+
+        if (str.find('.') == string::npos) {
+            return Node{stoi(str)};
+        }
+
+        return Node{stod(str)};
+    }
+
+    Node LoadBool(istream& input) {
+        string str;
+        char c;
+        input >> c;
+        while (isalpha(c)) {
+            str += c;
+            input >> c;
+        }
+
+        if (c == ',' || c == '}') {
+            input.putback(c);
+        }
+
+        if (str == "false") {
+            return Node{false};
+        }
+
+        return Node{true};
+    }
+
+    Node LoadNode(istream& input) {
+        char c;
+        input >> c;
+
+        if (c == '[') {
+            return LoadArray(input);
+        } else if (c == '{') {
+            return LoadDict(input);
+        } else if (c == '"') {
+            return LoadString(input);
+        } else if (isalpha(c)) {
+            input.putback(c);
+            return LoadBool(input);
+        } else {
+            input.putback(c);
+            return LoadVal(input);
+        }
+    }
+
+    Document Load(istream& input) {
+        return Document{LoadNode(input)};
+    }
+
+}  // namespace Json
---
+++ belts_cpp/04_brown_belt/06_week_FINAL/05_transport_guide_part_E/tests.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "test_runner.h"
+
+void TestAddStops();
+void TestDistancesTable();
+void TestAddBusRoutes();
+void TestRouteStatistics();
+void TestProcessRetrieveQueries();
+
+void TestAll();

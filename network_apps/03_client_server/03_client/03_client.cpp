2cb12e2fe181097ac5da486f911298cd6b6d5e01
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2024-08-17 19:55:29 +0300

    Client-server basics ...

--- network_apps/03_client_server/03_client/03_client.cpp
+++ network_apps/03_client_server/03_client/03_client.cpp
@@ -4,69 +4,95 @@
 
 #include <iostream>
 
-#include <sys/types.h>   // socket(...)
 #include <sys/socket.h>  // socket(...), recv(...)
-
 #include <cstdlib>       // exit(...)
 #include <netinet/in.h>  // struct sockaddr_in, struct in_addr, htons(...)
-#include <arpa/inet.h>   // inet_pton(...)
+//#include <arpa/inet.h>   // inet_pton(...)
 #include <cstdio>
 #include <unistd.h>      // close(...)
-
+#include <netdb.h>       // struct hostent *gethostbyname(const char *name);
+#include <cstring>       // std::strerror - analogue of std::perror()
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        cerr << "usage: a.out <IP_address> <port>" << endl;
-        perror("wrong number of arguments");
-        exit(EXIT_FAILURE);
-    }
+//    if (argc != 3) {
+//        cerr << "usage: a.out <server_IP_address> <server_port>" << endl;
+//        perror("wrong number of arguments");
+//        exit(EXIT_FAILURE);
+//    }
 
+    // ============== Connection parameters ==============
     const size_t MAX_BUFFER_LEN = 4096;  // max buffer size to hold a response from the server
+    char buffer[MAX_BUFFER_LEN];
+    const string srv_ip = "127.0.0.5";
+    const int srv_port = 43180;
 
-    // ============== create a socket ==============
-    int socket_fd;
-    if ( (socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket(...) error");
+    // ============== Create a socket ==============
+    int client_socket_fd;
+
+    // функция "socket" создает потоковый TCP-сокет (AF_INET - это красивое название для обычного сокета)
+    // SOCK_STREAM <=> TCP; SOCK_DGRAM <=> UDP
+    if ((client_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        cerr << "Creating socket error: " << strerror(errno) << endl;
         exit(EXIT_FAILURE);
     }
 
-    // specify an address for the socket; htons: host to network short
-    struct sockaddr_in srv_address{
+    struct hostent* hostinfo = nullptr;
+    hostinfo = gethostbyname(srv_ip.c_str());
+    if (hostinfo == nullptr) {
+        perror("gethostbyname(...) error");
+        exit(EXIT_FAILURE);
+    }
+
+    // specify an address for the socket (use designated initializers);
+    // htons: host to network short
+    // host might be connected to multiple networks and have different addresses on each one
+    struct sockaddr_in srv_addr{
         .sin_family = AF_INET,
-        .sin_port = htons(static_cast<uint16_t>(stoi(argv[2]))),
+        .sin_port = htons(srv_port),
+        .sin_addr = *(struct in_addr*) hostinfo->h_addr_list[0],
         .sin_zero = {}
     };
 
-    if (inet_pton(AF_INET, argv[1], &srv_address.sin_addr) <= 0) {
-        cerr << "inet_pton error for " << argv[1] << " IP address" << endl;
-        perror("inet_pton(...) error");
-        exit(EXIT_FAILURE);
-    }
+
+//    if (inet_pton(AF_INET, srv_ip.c_str(), &srv_addr.sin_addr) <= 0) {
+//        cerr << "Wrong server IP-address: " << strerror(errno) << endl;
+//        close(client_socket_fd);
+//        exit(EXIT_FAILURE);
+//    }
 
     // ================= connect ==================
-    int connection_status = connect(socket_fd,
-                                    (struct sockaddr*) &srv_address,
-                                    sizeof(srv_address));
+    int connection_status = connect(client_socket_fd,
+                                    (struct sockaddr*) &srv_addr,
+                                    sizeof(srv_addr));
     if (connection_status < 0) {
+        std::cerr << "Connect error: " << std::strerror(errno) << std::endl;
         cerr << "There was an error making a connection to the remote socket "
-             << argv[1] << ':' << argv[2] << endl;
-        perror("connect(...) error");
+             << srv_ip << ':' << srv_port << endl;
+        close(client_socket_fd);
         exit(EXIT_FAILURE);
     }
 
     // ============== receive data ================
-    char srv_response[MAX_BUFFER_LEN];
-//    ssize_t count = recv(socket_fd, &srv_response, MAX_BUFFER_LEN, 0); // receive data from the server
-    recv(socket_fd, &srv_response, MAX_BUFFER_LEN, 0); // receive data from the server
+    string srv_response;
+    ssize_t bytes_received;
+    while ((bytes_received = recv(client_socket_fd, buffer, sizeof(buffer), 0) ) > 0) {
+        srv_response.append(buffer, bytes_received);
+    }
+
+    if (bytes_received < 0) {
+        cerr << "Reading data error: " << strerror(errno) << endl;
+    } else {
+        cout << "Data received from the server " << srv_ip << ":" << srv_port
+             << ": " << srv_response << endl;
+    }
 
     // print out the server's response
-    printf("The server sent the data: %s\n", srv_response);
+    printf("The server sent the data: %s\n", buffer);
 
     // ============= close connection =============
-    // After we done, we close the socket with a close(...) function
-    close(socket_fd);
+    close(client_socket_fd);
 
     return 0;
 }
--- network_apps/03_client_server/03_server/03_server.cpp
+++ network_apps/03_client_server/03_server/03_server.cpp
@@ -14,45 +14,58 @@
 #include <unistd.h>      // close(...)
 
 #include <string>
+#include <cstring>       // std::strerror - analogue of std::perror()
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        cerr << "usage: " << argv[0] << " <port_to_listen_to>" << endl;
-        perror("wrong number of arguments");
-        exit(EXIT_FAILURE);
-    }
+//    if (argc != 2) {
+//        cerr << "usage: " << argv[0] << " <port_to_listen_to>" << endl;
+//        perror("wrong number of arguments");
+//        exit(EXIT_FAILURE);
+//    }
 
-    // So first we will create a string to hold the data that we're going to send all the clients
+//    So first we will create a string to hold the data that we're going to send all the clients
 //    const char* server_message = "You have reached the server";
+
     string server_message = "You have reached the server";
+    int server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    const int MAX_QUEUE_PENDING_CONNECTIONS_LEN = 10;
+//    const int port_listen_to = htons(static_cast<uint16_t>(stoi(argv[1])));
+    const int port_listen_to = 43180;
 
-    // create the server socker
-    int srv_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-    uint16_t port = htons(static_cast<uint16_t>(stoi(argv[1])));
-    // define the server address
     struct sockaddr_in srv_address = {
             .sin_family = AF_INET,
-            .sin_port = port,
+            .sin_port = port_listen_to,
             .sin_addr = {INADDR_ANY},
             .sin_zero = {}
     };
 
-    // bind the socket to out specified IP and port
-    bind(srv_socket, (struct sockaddr*) &srv_address, sizeof(srv_address));
+    // =============== Привязываем сокет к адресу и порту ===============
+    if (bind(server_socket_fd, reinterpret_cast<sockaddr*>(&srv_address), sizeof(srv_address)) < 0) {
+        cerr << "Socket bind error: " << strerror(errno) << std::endl;
+        close(server_socket_fd);
+        exit(EXIT_FAILURE);
+    }
 
-    listen(srv_socket, 5);
+    // =============== Начинаем прослушивание порта ===============
+    if ((listen(server_socket_fd, MAX_QUEUE_PENDING_CONNECTIONS_LEN)) < 0) {
+        cerr << "Listen error: " << strerror(errno) << endl;
+        close(server_socket_fd);
+        exit(EXIT_FAILURE);
+    }
+    cout << "Server is waiting connection on port " << port_listen_to << "..." << endl;
 
+    // =============== Принимаем соединение от клиента ===============
+    // TODO:
     int client_socket;
-    client_socket = accept(srv_socket, nullptr, nullptr);
+    client_socket = accept(server_socket_fd, nullptr, nullptr);
 
     // send the message
     send(client_socket, server_message.c_str(), sizeof(server_message.c_str()), 0);
 
     // close the socket
-    close(srv_socket);
+    close(server_socket_fd);
 
     return 0;
 }

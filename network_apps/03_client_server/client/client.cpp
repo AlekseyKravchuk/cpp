b870cf168bafc875b3f9a2b7ae73a1e520a73509
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2024-08-26 21:53:46 +0300

    network apps

---
+++ network_apps/03_client_server/client/client.cpp
@@ -0,0 +1,124 @@
+/*
+ * Simple TCP-client that prints out any data that it gets from a remote connection
+ */
+
+#include <iostream>
+
+#include <sys/socket.h>  // socket(...), recv(...)
+#include <cstdlib>       // exit(...)
+#include <netinet/in.h>  // struct sockaddr_in, struct in_addr, htons(...)
+#include <arpa/inet.h>   // inet_pton(...)
+#include <cstdio>
+#include <unistd.h>      // close(...)
+#include <netdb.h>       // struct hostent *gethostbyname(const char *name);
+#include <cstring>       // std::strerror - analogue of std::perror()
+
+using namespace std;
+
+/*
+ * Приложение, собирающееся использовать ТРАНСПОРТНЫЙ уровень для обмена данными, должно предоставить интерфейсу
+ * сокетов (socket)  следующую информацию:
+ * 1) указание на протокол, который надо использовать при обмене данными:
+ *        socket (AF_*, SOCK_STREAM, 0);  // 0 means 'auto choice' protocol number
+ *                  эквивалентно
+ *        socket (AF_*, SOCK_STREAM, 6);  // 6 means 'TCP' protocol inside IP packet
+ *
+ *    кроме потоковой передачи данных бывают ещё и другие:
+ *        SOCK_STREAM - bidirectional data streaming
+ *        SOCK_DGRAM  - unidirectional short messages (UDP)
+ *        SOCK_RAW    - IP level custom packets
+ *        SOCK_PACKET - Linux-only Ethernet-level custom frames
+ *  2) сетевой адрес другого абонента (IP-адрес)
+ *  3) номер порта, который будет использовать транспортный уровень
+ *  4) сами данные в виде массива байтов
+ */
+
+int main(int argc, char* argv[]) {
+    string srv_ip = "127.0.0.5";
+    uint16_t srv_port = 43180;
+
+    if (argc == 1) {
+        cout << "parameterless usage:"
+             << argv[0] << "  <server_IP_address> = " << srv_ip << " <server_port> = " << srv_port << endl;
+    } else if (argc == 3) {
+        srv_ip = argv[1];
+        srv_port = static_cast<uint16_t>(std::stoul(argv[2]));
+    } else {
+        cerr << "usage: " << argv[0] << " <server_IP_address> <server_port>" << endl
+             << "OR (parameterless):\n"
+             << "usage: " << argv[0] << endl;
+        perror("wrong number of arguments");
+        exit(EXIT_FAILURE);
+    }
+
+    // ============== Connection parameters ==============
+    const size_t MAX_BUFFER_LEN = 4096;  // max buffer size to hold a response from the server
+    char buffer[MAX_BUFFER_LEN];
+
+    // ============== Create a socket ==============
+    int client_socket_fd;
+
+    // функция "socket" создает потоковый TCP-сокет (AF_INET - это красивое название для обычного сокета)
+    // SOCK_STREAM <=> TCP; SOCK_DGRAM <=> UDP
+    if ((client_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        cerr << "Creating socket error: " << strerror(errno) << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    struct hostent* hostinfo = nullptr;
+    hostinfo = gethostbyname(srv_ip.c_str());
+    if (hostinfo == nullptr) {
+        perror("gethostbyname(...) error");
+        exit(EXIT_FAILURE);
+    }
+
+    // specify an address for the socket (use designated initializers);
+    // htons: host to network short
+    // host might be connected to multiple networks and have different addresses on each one
+    struct sockaddr_in srv_addr{
+        .sin_family = AF_INET,
+        .sin_port = htons(srv_port),
+        .sin_addr = *(struct in_addr*) hostinfo->h_addr_list[0],
+        .sin_zero = {}
+    };
+
+    if (inet_pton(AF_INET, srv_ip.c_str(), &srv_addr.sin_addr) <= 0) {
+        cerr << "Wrong server IP-address: " << strerror(errno) << endl;
+        close(client_socket_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // ================= connect ==================
+    int connection_status = connect(client_socket_fd,
+                                    (struct sockaddr*) &srv_addr,
+                                    sizeof(srv_addr));
+    if (connection_status < 0) {
+        std::cerr << "Connect error: " << std::strerror(errno) << std::endl;
+        cerr << "There was an error making a connection to the remote socket "
+             << srv_ip << ':' << srv_port << endl;
+        close(client_socket_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // ============== receive data ================
+    string srv_response;
+    ssize_t bytes_received;
+    while ((bytes_received = recv(client_socket_fd, buffer, sizeof(buffer), 0) ) > 0) {
+        srv_response.append(buffer, static_cast<size_t>(bytes_received));
+    }
+
+    if (bytes_received < 0) {
+        cerr << "Reading data error: " << strerror(errno) << endl;
+    } else {
+        cout << "Data received from the server " << srv_ip << ":" << srv_port
+             << ": " << srv_response << endl;
+    }
+
+    // print out the server's response
+    printf("The server sent the data: %s\n", buffer);
+
+    // ============= close connection =============
+    close(client_socket_fd);
+
+    return 0;
+}
---
+++ network_apps/03_client_server/server/server.cpp
@@ -0,0 +1,115 @@
+/*
+ * Simple TCP-server that listen on user-defined socket
+ */
+
+#include <iostream>
+
+#include <sys/types.h>   // socket(...)
+#include <sys/socket.h>  // socket(...), recv(...)
+
+#include <cstdlib>       // exit(...)
+#include <netinet/in.h>  // struct sockaddr_in, struct in_addr, htons(...)
+#include <arpa/inet.h>   // inet_pton(...)
+#include <cstdio>
+#include <unistd.h>      // close(...)
+
+#include <string>
+#include <cstring>       // std::strerror - analogue of std::perror()
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    //    const int port_listen_to = htons(static_cast<uint16_t>(stoi(argv[1])));
+    uint16_t port_listen_to = 43180;
+
+    // =============
+    if (argc == 1) {
+        cout << "parameterless usage:  " << argv[0] << R"( <port_listen_to=43180>)" << endl;
+    } else if (argc == 2) {
+        port_listen_to = static_cast<uint16_t>(std::stoul(argv[1]));
+    } else {
+        cerr << "usage: " << argv[0] << " <port_listen_to>" << endl << "OR\n"
+             << "usage: " << argv[0] << endl;
+        perror("wrong number of arguments");
+        exit(EXIT_FAILURE);
+    }
+
+    // Сообщение, которое будет передаваться всем клиентам при их подключении к серверу
+    string server_message = "You have reached the server";
+    const int MAX_QUEUE_PENDING_CONNECTIONS_LEN = 10;
+
+    // ====== Создаем серверный сокет (Listening Socket) =====
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0) {
+        cerr << "Socket creation error" << strerror(errno) << endl;
+        exit(EXIT_FAILURE);
+    }
+    // =======================================================
+
+    // ======== Настраиваем структуру адреса сервера =========
+    struct sockaddr_in server_address = {
+            .sin_family = AF_INET,
+            .sin_port = htons(port_listen_to),
+            .sin_addr = {INADDR_ANY},
+            .sin_zero = {}
+    };
+    // =======================================================
+
+    // ========== Привязываем сокет к адресу и порту =========
+    if (bind(server_sock, reinterpret_cast<sockaddr*>(&server_address), sizeof(server_address)) < 0) {
+        cerr << "Socket bind error: " << strerror(errno) << std::endl;
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    // =============== Начинаем прослушивание порта ===============
+    if ((listen(server_sock, MAX_QUEUE_PENDING_CONNECTIONS_LEN)) < 0) {
+        cerr << "Listen error: " << strerror(errno) << endl;
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+    cout << "Server is waiting connection on port " << port_listen_to << "..." << endl;
+
+    // =============== Принимаем соединение от клиента ===============
+    sockaddr_in client_address{};
+    socklen_t client_address_len = sizeof(client_address);
+
+    int client_sock = accept(server_sock,
+                             reinterpret_cast<sockaddr*>(&client_address),
+                             &client_address_len);
+    if (client_sock < 0) {
+        cerr << "Accept connection error: " << strerror(errno) << endl;
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+    cout << "Client is connected!" << endl;
+
+    // =============== Читаем данные от клиента ===============
+    string received_data;
+    char buffer[1024];
+    ssize_t bytes_received;
+
+    while( (bytes_received = recv(client_sock, buffer, sizeof(buffer), 0)) > 0) {
+        received_data.append(buffer, static_cast<size_t>(bytes_received));
+    }
+
+    if (bytes_received < 0) {
+        cerr << "Reading data error (recv): " << strerror(errno) << endl;
+    } else {
+        cout << "Following message is received from client: " << received_data << endl;
+    }
+
+    // =============== Отправляем ответ клиенту ===============
+    const string response = "Hello, Client!";
+    if (send(client_sock, response.c_str(), response.size(), 0) < 0) {
+        cerr << "Data sending error: " << strerror(errno) << endl;
+    }
+
+    // Закрываем соединение с клиентом
+    close(client_sock);
+
+    // Закрываем серверный сокет
+    close(server_sock);
+
+    return 0;
+}

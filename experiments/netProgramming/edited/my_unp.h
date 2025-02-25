a7d3e943d37f2d4c8fd9500ec96750b4cf684aa2
AlekseyKravchuk <aleksey-kravchuk@outlook.com>
2025-02-25 17:12:34 +0300

    Added simple http-server.

--- experiments/netProgramming/edited/my_unp.h
+++
@@ -1,8 +0,0 @@
-#ifndef MY_UNP_H
-#define MY_UNP_H
-
-#define	MAXLINE		4096	/* max text line length */
-#include <sys/socket.h> // Needed for socket creating and binding
-#include <netinet/in.h> // Needed to use struct sockaddr_in
-
-#endif	/* MY_UNP_H */
